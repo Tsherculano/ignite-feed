189ba4e2ac126ee7aaf0b48973f25d81cf051ad1
Tsherculano <tico.herculano@gmail.com>
2022-07-03 15:44:42 -0300

    style: Post stylization complete

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,26 +7,33 @@ export function Post() {
         <div className={styles.author}>
           <img
             className={styles.avatar}
-            src="https://github.com/Tsherculano.png" />
-          <div>
+            src="https://github.com/Tsherculano.png"
+          />
+          <div className={styles.authorInfo}>
             <strong>Thiago Herculano</strong>
             <span>Web Developer</span>
           </div>
         </div>
 
-        <time title="03 de Julho às 13:10hs " dateTime="2022-07-03 13:10:00">Publicado há 1h</time>
+        <time title="03 de Julho às 13:10hs " dateTime="2022-07-03 13:10:00">
+          Publicado há 1h
+        </time>
       </header>
 
       <div className={styles.content}>
         <p>Fala galeraa 👋</p>
 
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-        <p>👉 <a href="">jane.design/doctorcare</a></p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
 
         <p>
-          <a href="">#novoprojetot</a>
-          <a href="">#nlw</a>
+          👉 <a href="">jane.design/doctorcare</a>
+        </p>
+
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{' '}
           <a href="">#rocketseat</a>
         </p>
       </div>
